246b38dbe337e4bcefa6a9f258e765abf945ef92
dud <Heo250113@gmail.com>
2025-03-07 20:26:25 +0900

    refactor: 프로젝트 인터페이스가 ISP를 준수하도록 분리 (2025.03.07) - 기간: 2025.03.07 - 학습내용: Readable Code: 읽기 좋은 코드를 작성하는 사고법 Section 4.8.ISP:Interface Segregation Principle

---
+++ src/main/java/cleancode/minesweeper/tobe/AnotherGame.java
@@ -0,0 +1,16 @@
+package cleancode.minesweeper.tobe;
+
+import cleancode.minesweeper.tobe.game.GameRunnable;
+
+public class AnotherGame implements GameRunnable {
+
+    //    @Override
+//    public void initialize() {
+//        // 필요없는 기능 ISP 위반
+//    }
+
+    @Override
+    public void run() {
+        // 게임 진행
+    }
+}
