57d5c0ee7f4ae6f94a3a75c1323bbac776521513
dud <Heo250113@gmail.com>
2025-03-09 01:57:38 +0900

    refactor: 상속 받던 기능을 인터페이스를 사용하도록 설정 (2025.03.09) - 기간: 2025.03.09 - 학습내용: Readable Code: 읽기 좋은 코드를 작성하는 사고법 Section 5. 상속과 조합

--- src/main/java/cleancode/minesweeper/tobe/cell/LandMindCell.java
+++ src/main/java/cleancode/minesweeper/tobe/cell/LandMindCell.java
@@ -1,10 +1,10 @@
 package cleancode.minesweeper.tobe.cell;
 
-public class LandMindCell extends Cell {
+public class LandMindCell implements Cell {
 
     private static final String LAND_MINE_SIGN = "☼";
 
-    private boolean isLandMine;
+    private final CellState cellState = CellState.initialize();
 
 
     @Override
@@ -19,12 +19,32 @@ public class LandMindCell extends Cell {
 
     @Override
     public String getSign() {
-        if(isOpened){
+        if(cellState.isOpened()){
             return LAND_MINE_SIGN;
         }
-        if(isFlagged){
+        if(cellState.isFlagged()){
             return FLAG_SIGN;
         }
         return UNCHECKED_SIGN;
+    }
+
+    @Override
+    public void flag() {
+        cellState.flag();
+    }
+
+    @Override
+    public void open() {
+        cellState.open();
+    }
+
+    @Override
+    public boolean isChecked() {
+        return cellState.isChecked();
+    }
+
+    @Override
+    public boolean isOpened() {
+        return cellState.isOpened();
     }
 }
