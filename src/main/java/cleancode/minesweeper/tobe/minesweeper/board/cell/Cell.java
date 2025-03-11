008ce43e7338621ef797496ba71a4d17ec834cf8
dud <Heo250113@gmail.com>
2025-03-11 11:57:55 +0900

    refactor: 폴더 구조 변경 (2025.03.11) - 기간: 2025.03.11 - 학습내용: Readable Code: 읽기 좋은 코드를 작성하는 사고법 Section 6.3.패키지 나누기

---
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/Cell.java
@@ -0,0 +1,26 @@
+package cleancode.minesweeper.tobe.minesweeper.board.cell;
+
+public interface Cell {
+
+
+    String FLAG_SIGN = "⚑";
+    String UNCHECKED_SIGN = "□";
+
+   boolean isLandMine();
+
+    boolean hasLandMineCount();
+
+    CellSnapshot getSnapshot();
+
+    String getSign() ;
+
+    void flag();
+
+    void open();
+
+    boolean isChecked();
+
+    boolean isOpened();
+
+
+}
---
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/LandMineCell.java
@@ -0,0 +1,61 @@
+package cleancode.minesweeper.tobe.minesweeper.board.cell;
+
+public class LandMineCell implements Cell {
+
+    private static final String LAND_MINE_SIGN = "☼";
+
+    private final CellState cellState = CellState.initialize();
+
+
+    @Override
+    public boolean isLandMine() {
+        return true;
+    }
+
+    @Override
+    public boolean hasLandMineCount() {
+        return false;
+    }
+
+    @Override
+    public CellSnapshot getSnapshot() {
+        if (cellState.isOpened()) {
+            return CellSnapshot.ofLandMine();
+        }
+        if (cellState.isFlagged()) {
+            return CellSnapshot.ofFlag();
+        }
+        return CellSnapshot.ofUnchecked();
+    }
+
+    @Override
+    public String getSign() {
+        if(cellState.isOpened()){
+            return LAND_MINE_SIGN;
+        }
+        if(cellState.isFlagged()){
+            return FLAG_SIGN;
+        }
+        return UNCHECKED_SIGN;
+    }
+
+    @Override
+    public void flag() {
+        cellState.flag();
+    }
+
+    @Override
+    public void open() {
+        cellState.open();
+    }
+
+    @Override
+    public boolean isChecked() {
+        return cellState.isChecked();
+    }
+
+    @Override
+    public boolean isOpened() {
+        return cellState.isOpened();
+    }
+}
